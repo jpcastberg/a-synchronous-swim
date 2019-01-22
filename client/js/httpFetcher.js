1fff0568713a635cc785cfac15b7c3f4fcc448b0
John Castberg <jpcastberg@gmail.com>
2019-01-22 10:56:08 -0800

    Update index.html to access js and css files, add move button, begin on http req

--- client/js/httpFetcher.js
+++ client/js/httpFetcher.js
@@ -1,5 +1,17 @@
 (function() {
 
-  const serverUrl = '?';
+  const serverUrl = 'http://127.0.0.1:3000';
+  
+  $.ajax({
+    url: serverUrl,
+    type: 'GET',
+    contentType: '?',
+    success: function(data) {
+      console.log('Success! ', data);
+    },
+    error: function(error) {
+      console.error('Error getting data from the server: ', error);
+    }
+  });
 
 })();
--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -2,5 +2,6 @@ const headers = require('./cors');
 
 module.exports = (req, res) => {
   res.writeHead(200, headers);
+  res.write(JSON.stringify({command: 'down'}))
   res.end();
 };
