2fab3e27f2d92170786431fc91b63bff0436fc6d
John Castberg <jpcastberg@gmail.com>
2019-01-22 20:18:30 -0800

    Fully implements server side control

--- client/js/httpFetcher.js
+++ client/js/httpFetcher.js
@@ -7,16 +7,19 @@ var fetchCommand = function() {
     type: 'GET',
     contentType: '?',
     success: function(data) {
-      console.log('Success! ', data);
       var command = JSON.parse(data).command;
       SwimTeam.move(command);
     },
     error: function(error) {
       console.error('Error getting data from the server: ', error);
+    },
+    complete: function() {
+      setTimeout(fetchCommand, 25);
     }
   });
-
 };
+
+fetchCommand();
 
 //   // Create WebSocket connection.
 // const socket = new WebSocket('ws://localhost:8080');
